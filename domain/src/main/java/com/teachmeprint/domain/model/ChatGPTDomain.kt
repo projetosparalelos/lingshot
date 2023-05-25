2a5407087f130b1c895999732b3c3eb972fffd32
chamsilva <charles.m.silva@mercadolivre.com>
2023-05-25 20:36:54 -0300

    Update domain.

--- domain/src/main/java/com/teachmeprint/domain/model/ChatGPTDomain.kt
+++ domain/src/main/java/com/teachmeprint/domain/model/ChatGPTDomain.kt
@@ -8,11 +8,10 @@ data class ChatGPTPromptBodyDomain(
     val topP: Float? = TOP_P_DEFAULT,
     val frequencyPenalty: Int? = 0,
     val presencePenalty: Int? = 0,
-    val stop: String? = STOP_DEFAULT
+    val stop: String? = null
 )
 
 private const val MODEL_DEFAULT = "text-davinci-003"
 private const val MAX_TOKENS_DEFAULT = 100
 private const val TEMPERATURE_DEFAULT = 0.3f
 private const val TOP_P_DEFAULT = 1F
-private const val STOP_DEFAULT = "\n"
