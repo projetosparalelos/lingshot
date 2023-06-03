ccb208567acae8828a0fcf30cae95b1e6e01c2a3
chamsilva <charles.m.silva@mercadolivre.com>
2023-06-02 21:42:50 -0300

    Update ui bottom sheet.

--- feature/screenshot/screenshot_presentation/src/main/java/com/teachmeprint/screenshot_presentation/ui/component/ScreenShotTranslateBottomSheet.kt
+++ feature/screenshot/screenshot_presentation/src/main/java/com/teachmeprint/screenshot_presentation/ui/component/ScreenShotTranslateBottomSheet.kt
@@ -1,30 +1,34 @@
 package com.teachmeprint.screenshot_presentation.ui.component
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Check
-import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material.icons.filled.SubdirectoryArrowRight
+import androidx.compose.material.icons.filled.Translate
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
+import com.google.accompanist.placeholder.PlaceholderHighlight
+import com.google.accompanist.placeholder.material.placeholder
+import com.google.accompanist.placeholder.material.shimmer
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,42 +38,62 @@ fun ScreenShotTranslateBottomSheet(
     onDismiss: () -> Unit
 ) {
     val sheetState = rememberModalBottomSheetState()
-    val scope = rememberCoroutineScope()
 
     ModalBottomSheet(
         modifier = modifier,
         sheetState = sheetState,
         onDismissRequest = onDismiss
     ) {
-        Row(horizontalArrangement = Arrangement.SpaceAround) {
-            CenterAlignedTopAppBar(navigationIcon = {
-                IconButton(onClick = {
-                    scope.launch { sheetState.hide() }.invokeOnCompletion {
-                        onDismiss()
-                    }
-                }) {
-                    Icon(Icons.Rounded.Close, contentDescription = "Cancel")
-                }
-            }, title = {
-                    Text(
-                        "Your translate",
-                        color = MaterialTheme.colorScheme.secondary
-                    )
-                }, actions = {
-                    IconButton(onClick = { }) {
-                        Icon(Icons.Rounded.Check, contentDescription = "Save")
-                    }
-                })
-        }
         Column(
             modifier = Modifier.padding(horizontal = 16.dp),
             horizontalAlignment = Alignment.Start
         ) {
             Text(
-                text = text.orEmpty(),
-                fontSize = 16.sp,
-                color = MaterialTheme.colorScheme.primary
+                "Your translate",
+                style = MaterialTheme.typography.titleLarge
             )
+            Spacer(modifier = Modifier.height(24.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(42.dp)
+                        .background(MaterialTheme.colorScheme.primary, CircleShape)
+                        .padding(12.dp),
+                    imageVector = Icons.Default.Translate,
+                    tint = MaterialTheme.colorScheme.onPrimary,
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Column {
+                    Text(
+                        text = text.orEmpty(),
+                        fontSize = 16.sp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            modifier = Modifier.size(20.dp),
+                            imageVector = Icons.Default.SubdirectoryArrowRight,
+                            contentDescription = null
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(
+                            modifier = Modifier
+                                .widthIn(min = 100.dp)
+                                .placeholder(
+                                    visible = true,
+                                    highlight = PlaceholderHighlight.shimmer()
+                                ),
+                            text = "",
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+            }
             Spacer(modifier = Modifier.height(16.dp))
         }
     }
