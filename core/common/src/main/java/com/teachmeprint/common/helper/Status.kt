06435dc27d06b0d95d57308f67718ab1f21d007f
chamsilva <charles.m.silva@mercadolivre.com>
2023-06-04 14:48:49 -0300

    Add text of the button in string.xml.

--- core/common/src/main/java/com/teachmeprint/common/helper/Status.kt
+++ core/common/src/main/java/com/teachmeprint/common/helper/Status.kt
@@ -2,6 +2,7 @@ package com.teachmeprint.common.helper
 
 import androidx.compose.runtime.Composable
 import com.teachmeprint.common.helper.StatusMessage.STATUS_TEXT_ERROR_GENERIC
+import java.io.IOException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
@@ -46,10 +47,15 @@ fun <T> CoroutineScope.launchWithStatus(
         try {
             val value = data()
             onCopy(Status.Success(value))
-        } catch (e: HttpException) {
-            onCopy(Status.Error(e.code()))
         } catch (e: Exception) {
-            onCopy(Status.Error(STATUS_TEXT_ERROR_GENERIC))
+            when (e) {
+                is HttpException -> {
+                    onCopy(Status.Error(e.code()))
+                }
+                else -> {
+                    onCopy(Status.Error(STATUS_TEXT_ERROR_GENERIC))
+                }
+            }
         }
     }
 }
