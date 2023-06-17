f1075235fb38b1ff27c464c295f39b410b9147c2
chamsilva <charles.m.silva@mercadolivre.com>
2023-06-17 11:12:04 -0300

    Update plugins.

--- plugins/settings.gradle.kts
+++ plugins/settings.gradle.kts
@@ -4,9 +4,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven {
-            url = uri("https://plugins.gradle.org/m2/")
-        }
+
     }
     versionCatalogs {
         create("libs") {
