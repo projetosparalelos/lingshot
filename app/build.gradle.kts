7f3538f7300bb1da9e404b588f97a3d92cfa4d2b
chamsilva <charles.m.silva@mercadolivre.com>
2023-06-23 20:05:23 -0300

    Add libs auth.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.hilt.navigation.compose)
 
     implementation(libs.play.services.ads)
+    implementation(libs.play.services.auth)
     implementation(platform(libs.firebase.bom))
     implementation(libs.bundles.firebase)
 }
