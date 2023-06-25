645b1c4a11e5cc75ad1d24c00bf091c2152f3309
chamsilva <charles.m.silva@mercadolivre.com>
2023-06-25 19:24:39 -0300

    Add toolbar with user profile.

--- app/src/main/java/com/teachmeprint/language/presentation/ui/MainScreen.kt
+++ app/src/main/java/com/teachmeprint/language/presentation/ui/MainScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -91,6 +92,7 @@ private fun MainScreen(
                 title = {
                     Text(
                         text = "Hi, ${uiState.userDomain?.firstName}",
+                        style = MaterialTheme.typography.titleMedium,
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis
                     )
