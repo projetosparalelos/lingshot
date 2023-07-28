4cb7d7b7da32a1159ad9c3dca5e8dc0dd11100d6
chamsilva <charles.m.silva@mercadolivre.com>
2023-07-28 17:16:18 -0300

    Add nothing.

--- .github/set-key.sh
+++ .github/set-key.sh
@@ -6,4 +6,3 @@ echo "ADMOB_APP_ID_DEBUG=\"$ADMOB_APP_ID_DEBUG\"" >> ./lingshot-keys.properties
 echo "ADMOB_INTERSTITIAL_ID_RELEASE=\"$ADMOB_INTERSTITIAL_ID_RELEASE\"" >> ./lingshot-keys.properties
 echo "ADMOB_INTERSTITIAL_ID_DEBUG=\"$ADMOB_INTERSTITIAL_ID_DEBUG\"" >> ./lingshot-keys.properties
 echo "$FIREBASE_GOOGLE_SERVICES" >> ./app/src/google-services.json
-echo "$RELEASE_KEY" | base64 -di >> ./lingshot-key
