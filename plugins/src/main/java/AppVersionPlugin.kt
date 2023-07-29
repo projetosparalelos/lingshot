bdc63c373429dbba492bb7abf95461f9e06b9ee1
chamsilva <charles.m.silva@mercadolivre.com>
2023-07-29 11:42:38 -0300

    Add logic to create tagname.

--- plugins/src/main/java/AppVersionPlugin.kt
+++ plugins/src/main/java/AppVersionPlugin.kt
@@ -31,7 +31,7 @@ class AppVersionPlugin : Plugin<Project> {
 
   companion object {
     const val APPLICATION_NAME_ID = "com.lingshot.language"
-    const val VERSION_CODE = 2
+    const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
 
     const val COMPILE_SDK = 33
