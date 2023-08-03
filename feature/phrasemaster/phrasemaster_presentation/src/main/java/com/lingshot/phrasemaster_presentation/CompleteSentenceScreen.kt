1d29af5135f1fd53333432ca1b5d3a137a9ded44
chamsilva <charles.m.silva@mercadolivre.com>
2023-08-03 08:19:33 -0300

    Update screen.

--- feature/phrasemaster/phrasemaster_presentation/src/main/java/com/lingshot/phrasemaster_presentation/CompleteSentenceScreen.kt
+++ feature/phrasemaster/phrasemaster_presentation/src/main/java/com/lingshot/phrasemaster_presentation/CompleteSentenceScreen.kt
@@ -2,21 +2,30 @@
 
 package com.lingshot.phrasemaster_presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,47 +35,84 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun CompleteSentenceScreen() {
-    val sentence = "How are you doing? You ((eating)) today?"
+    val sentence = "How are ((you)) doing?"
     val sentenceWithoutParentheses = sentence.replace(Regex("\\(\\((.*?)\\)\\)"), "$1")
     val getWordWithoutParentheses = extractWordsInDoubleParentheses(sentence)
     val parts = sentenceWithoutParentheses.split(getWordWithoutParentheses)
     var userInput by remember { mutableStateOf(TextFieldValue()) }
-
-    FlowRow(
-        modifier = Modifier.padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
+    val focusRequester = remember { FocusRequester() }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        parts.forEachIndexed { index, part ->
-            Text(
-                text = part,
-                style = MaterialTheme.typography.bodyMedium,
-                color = Color.Black
+        ElevatedCard(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.onSecondary
             )
-            if (index < parts.size - 1) {
-                val isError = userInput.text.isNotEmpty() && !userInput.text.startsWith(
-                    getWordWithoutParentheses.substring(0, userInput.text.length)
-                )
-
-                MeasureUnconstrained(viewToMeasure = {
+        ) {
+            FlowRow(
+                modifier = Modifier.padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                parts.forEachIndexed { index, part ->
                     Text(
-                        text = getWordWithoutParentheses,
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding(16.dp)
+                        text = part,
+                        style = MaterialTheme.typography.headlineSmall,
+                        color = MaterialTheme.colorScheme.primary
                     )
-                }) { measuredWidth, measuredHeight ->
-                    OutlinedTextField(
-                        isError = isError,
-                        value = userInput,
-                        onValueChange = {
-                            userInput = it
-                        },
-                        textStyle = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier
-                            .size(width = measuredWidth, height = measuredHeight)
-                    )
+                    if (index < parts.size - 1) {
+                        val isError = userInput.text.isNotEmpty() && !userInput.text.startsWith(
+                            getWordWithoutParentheses.substring(0, userInput.text.length)
+                        )
+
+                        MeasureUnconstrained(viewToMeasure = {
+                            Text(
+                                text = getWordWithoutParentheses,
+                                style = MaterialTheme.typography.headlineSmall,
+                                modifier = Modifier.padding(17.dp)
+                            )
+                        }) { measuredWidth, measuredHeight ->
+                            OutlinedTextField(
+                                modifier = Modifier
+                                    .focusRequester(focusRequester)
+                                    .size(width = measuredWidth, height = measuredHeight),
+                                isError = isError,
+                                singleLine = true,
+                                value = userInput,
+                                onValueChange = {
+                                    if (it.text.length <= getWordWithoutParentheses.length) {
+                                        userInput = it
+                                    }
+                                },
+                                textStyle = MaterialTheme.typography.headlineSmall.copy(
+                                    MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
+                                )
+                            )
+                        }
+                    }
                 }
             }
         }
+
+        ElevatedCard(
+            modifier = Modifier
+                .fillMaxWidth(),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.onSecondary
+            )
+        ) {
+            Box(modifier = Modifier.padding(16.dp)) {
+                Text(text = "Como você está?", style = MaterialTheme.typography.bodyLarge)
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
     }
 }
 
@@ -98,7 +144,7 @@ fun MeasureUnconstrained(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun CompleteSentenceScreenPreview() {
     CompleteSentenceScreen()
