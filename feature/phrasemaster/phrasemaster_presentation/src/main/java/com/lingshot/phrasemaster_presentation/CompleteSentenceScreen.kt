6b45682d4615106abe3b56c409d6073de9e2f44c
chamsilva <charles.m.silva@mercadolivre.com>
2023-08-03 10:44:49 -0300

    Add new textfield.

--- feature/phrasemaster/phrasemaster_presentation/src/main/java/com/lingshot/phrasemaster_presentation/CompleteSentenceScreen.kt
+++ feature/phrasemaster/phrasemaster_presentation/src/main/java/com/lingshot/phrasemaster_presentation/CompleteSentenceScreen.kt
@@ -2,6 +2,7 @@
 
 package com.lingshot.phrasemaster_presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,10 +12,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,6 +41,7 @@ fun CompleteSentenceScreen() {
     val sentenceWithoutParentheses = removeDoubleParenthesesAllPhrase(sentence)
     val getWordWithoutParentheses = extractWordsInDoubleParentheses(sentence)
     val parts = sentenceWithoutParentheses.split(getWordWithoutParentheses)
+
     var userInput by remember { mutableStateOf(TextFieldValue()) }
     val focusRequester = remember { FocusRequester() }
     Column(
@@ -58,39 +61,49 @@ fun CompleteSentenceScreen() {
                 modifier = Modifier.padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
+                val textTypography = MaterialTheme.typography.headlineSmall
+                val textColor = MaterialTheme.colorScheme.primary
+
                 parts.forEachIndexed { index, part ->
                     Text(
                         text = part,
-                        style = MaterialTheme.typography.headlineSmall,
-                        color = MaterialTheme.colorScheme.primary
+                        style = textTypography,
+                        color = textColor
                     )
                     if (index < parts.size - 1) {
                         val isError = userInput.text.isNotEmpty() && !userInput.text.startsWith(
                             getWordWithoutParentheses.substring(0, userInput.text.length)
                         )
 
+                        val colorBasicTextField = if (isError) {
+                            MaterialTheme.colorScheme.error
+                        } else {
+                            textColor.copy(alpha = 0.8f)
+                        }
+
                         LingshotMeasureUnconstrained(viewToMeasure = {
                             Text(
                                 text = getWordWithoutParentheses,
-                                style = MaterialTheme.typography.headlineSmall,
-                                modifier = Modifier.padding(17.dp)
+                                style = textTypography,
+                                modifier = Modifier.padding(horizontal = 5.dp)
                             )
                         }) { measuredWidth, measuredHeight ->
-                            OutlinedTextField(
+                            BasicTextField(
                                 modifier = Modifier
                                     .focusRequester(focusRequester)
-                                    .size(width = measuredWidth, height = measuredHeight),
-                                isError = isError,
-                                singleLine = true,
+                                    .background(MaterialTheme.colorScheme.surfaceVariant)
+                                    .padding(4.dp)
+                                    .size(width = measuredWidth, height = measuredHeight)
+                                    .padding(horizontal = 4.dp),
                                 value = userInput,
+                                singleLine = true,
+                                textStyle = textTypography.copy(color = colorBasicTextField),
+                                cursorBrush = SolidColor(colorBasicTextField),
                                 onValueChange = {
                                     if (it.text.length <= getWordWithoutParentheses.length) {
                                         userInput = it
                                     }
-                                },
-                                textStyle = MaterialTheme.typography.headlineSmall.copy(
-                                    MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
-                                )
+                                }
                             )
                         }
                     }
