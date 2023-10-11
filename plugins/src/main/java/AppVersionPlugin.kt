fbc01748ea0828287a769fc9f2b353d34b36e41c
chamsilva <charles.m.silva@mercadolivre.com>
2023-10-10 23:13:19 -0300

    Add singleton manager to MainActivity.

--- plugins/src/main/java/AppVersionPlugin.kt
+++ plugins/src/main/java/AppVersionPlugin.kt
@@ -43,8 +43,8 @@ class AppVersionPlugin : Plugin<Project> {
 
   companion object {
     const val APPLICATION_NAME_ID = "com.lingshot.languagelearn"
-    const val VERSION_CODE = 6
-    const val VERSION_NAME = "1.0.6"
+    const val VERSION_CODE = 7
+    const val VERSION_NAME = "1.0.7"
 
     const val COMPILE_SDK = 34
     const val TARGET_SDK = 34
